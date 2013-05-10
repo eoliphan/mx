9214f1b7ba12c142eef56779b34fa820e968b074
Erich Oliphant <erich.oliphant@gmail.com>
2013-05-10 17:51:47 -0400

    MUSX-47 New FAQ page per https://osedev.jira.com/wiki/display/MUSX/FAQs and it's subpages

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,8 @@ module.exports = function(app){
     require("./order")(app);
     require("./wager")(app);
     require("./album")(app);
+    app.get("/faq",function(req,res){
+       res.render('faq',{title:"FAQ"});
+    });
     return {};
 }
